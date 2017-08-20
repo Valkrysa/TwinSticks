c75bf7e317533a0d726a6ac236b63fa4d2287ad7
Alice Robinson <valkrysa@gmail.com>
2017-08-20 15:36:39 -0700

    Through 276, completed the replay system case where not enough frames exist.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    public bool recording = true;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1")) {
+            recording = false;
+        }
+        if (CrossPlatformInputManager.GetButtonUp("Fire1")) {
+            recording = true;
+        }
+    }
+}
---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+    private GameManager myGameManager;
+    private Rigidbody myRigidBody;
+    private int currentFrame = -1;
+    private int maxFrame = -1;
+    private const int bufferFrames = 1000;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody>();
+        myGameManager = GameObject.FindObjectOfType<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentFrame += 1;
+        if (currentFrame > maxFrame) {
+            maxFrame = currentFrame;
+        }
+        if (currentFrame == bufferFrames) {
+            currentFrame = 0;
+        }
+        if (myGameManager.recording) {
+            Record();
+        } else {
+            PlayBack();
+        }
+    }
+
+    private void PlayBack () {
+        if (currentFrame == maxFrame) {
+            currentFrame = 0;
+        }
+        myRigidBody.isKinematic = true;
+        int frame = currentFrame % maxFrame;
+        Debug.Log(currentFrame + " % " + maxFrame + " = " + frame);
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+
+    private void Record() {
+        myRigidBody.isKinematic = false;
+
+        keyFrames[currentFrame] = new MyKeyFrame(transform.position, transform.rotation);
+    }
+}
+
+/// <summary>
+/// A structure for storing position and rotation.
+/// </summary>
+public struct MyKeyFrame {
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame (Vector3 pos, Quaternion rot) {
+        position = pos;
+        rotation = rot;
+    }
+}
